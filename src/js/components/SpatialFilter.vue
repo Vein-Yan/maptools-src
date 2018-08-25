37c97eb6a6d97224d0d529ee59d0f851d2d5d8f7
vein <trojan_ann@aliyun.com>
2018-08-25 19:06:22 +0800

    增加分页，空间过滤按钮

--- src/js/components/SpatialFilter.vue
+++ src/js/components/SpatialFilter.vue
@@ -5,24 +5,30 @@
       <el-breadcrumb-item>空间过滤</el-breadcrumb-item>
     </el-breadcrumb>
     <div v-if="tableData.length>0" class="v-table">
-      <el-table
-      :data="tableData"
-      stripe
-      border
-      style="width: 100%">
+      <div class="v-filter-btn-container">
+        <el-button class="v-filter-btn" type="primary" icon="fa fa-filter" round>空间过滤</el-button>
+        <el-tooltip class="v-filter-btn-circle" content="Top center" placement="top">
+          <el-button type="primary" icon="fa fa-filter" circle></el-button>
+        </el-tooltip>
+      </div>
+      <el-table :data="currentPageData" stripe border style="width: 100%">
         <div v-for="column in tableColumns">
-          <el-table-column  :prop="column" :label="column" align="left"></el-table-column>
+          <el-table-column :prop="column" :label="column" align="left"></el-table-column>
         </div>
       </el-table>
+      <div class="v-block">
+        <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange" :current-page="currentPage" :page-sizes="[10, 20, 50, 100, 150]" :page-size="pageSize" layout="total, sizes, prev, pager, next, jumper" :total="tableData.length">
+        </el-pagination>
+      </div>
     </div>
     <div v-if="tableData.length===0" class="v-upload">
       <el-upload class="upload-demo" drag :multiple="false" accept="text/csv" action="https://jsonplaceholder.typicode.com/posts/" :on-change=onChange>
-      <i class="el-icon-upload"></i>
-      <div class="el-upload__text">将文件拖到此处，或
-        <em>点击上传</em>
-      </div>
-      <div class="el-upload__tip" slot="tip">只支持csv文件</div>
-    </el-upload>
+        <i class="el-icon-upload"></i>
+        <div class="el-upload__text">将文件拖到此处，或
+          <em>点击上传</em>
+        </div>
+        <div class="el-upload__tip" slot="tip">只支持csv文件</div>
+      </el-upload>
     </div>
   </div>
 </template>
@@ -30,43 +36,82 @@
 <script>
 import Papa from 'papaparse'
 export default {
-  data(){
+  data() {
     return {
+      currentPage: 1,
+      pageSize: 10,
       tableColumns: null,
       tableData: []
+    }
+  },
+  computed: {
+    currentPageData() {
+      let pageSize = this.pageSize,
+        currentPage = this.currentPage
+      let currentPageData = []
+      var maxIndex = this.tableData.length - 1
+      let start = (currentPage - 1) * pageSize,
+        end = currentPage * pageSize - 1
+      end = end > maxIndex ? maxIndex : end
+      for (let i = start; i <= end; i++) {
+        currentPageData.push(this.tableData[i])
+      }
+      return currentPageData
     }
   },
   methods: {
     onChange(file) {
       Papa.parse(file.raw, {
-        complete: (result) => {
+        complete: result => {
           let data = result.data
           this.tableData.length = 0
           this.tableColumns = data[0]
-          data.forEach((element,i) => {
-            if(i === 0){
+          data.forEach((element, i) => {
+            if (i === 0) {
               return
             }
             let rowData = data[i],
-            row = {}
+              row = {}
             this.tableColumns.forEach((ele, idx) => {
               row[ele] = rowData[idx]
-            });
+            })
             this.tableData.push(row)
-          });
+          })
         }
       })
+    },
+    handleSizeChange(val) {
+      this.pageSize = val
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val
     }
   }
 }
 </script>
 
 <style>
-.v-table{
+.v-table {
   margin-top: 16px;
 }
-.v-upload{
+.v-upload {
   margin-top: 100px;
+}
+.v-block {
+  margin-top: 12px;
+}
+.v-filter-btn-container{
+  text-align: right;
+}
+.v-filter-btn {
+  position: relative;
+  top: -16px;
+}
+.v-filter-btn-circle {
+  position: absolute;
+  top: 87px;
+  right: 123px;
+  z-index: 99;
 }
 </style>
 
